5a8bc14f2f6a4b2ec0b1d7d5f7bd9d7b7737ec43
neo1seok <neo1seok@gmail.com>
2016-12-09 16:46:41 +0900

    a

--- businfo/runbus.py
+++ businfo/runbus.py
@@ -1,8 +1,11 @@
 import pymysql
 import neolib.neolib as neolib
+import neolib.db as neodb
+import time
+
 import  simplejson as json
 import re
-
+import collections
 class TestOpeAPI(neolib.NeoRunnableClasss):
 	def doRun(self):
 		from urllib.request import Request, urlopen
@@ -26,41 +29,216 @@ class TestOpeAPI(neolib.NeoRunnableClasss):
 		print(response_body.decode())
 
 		print('test')
+
+
 class MakeJsonFormFromText(neolib.NeoRunnableClasss):
+	# filelist = [
+	# 	'area20161202.txt',
+	# 	'route20161202.txt',
+	# 	'routeline20161202.txt',
+	# 	'routestation20161202.txt',
+	# 	'station20161202.txt']
 	filelist = [
-		'area20161202.txt',
-		'route20161202.txt',
-		'routeline20161202.txt',
-		'routestation20161202.txt',
-		'station20161202.txt']
+		'area',
+		'route',
+		'routeline',
+		'routestation',
+		'station']
+
+	def makeRows(self,rsrc):
+		str = neolib.StrFromFile(rsrc, enc='euc-kr')
+		cmps = [tmp.split('|') for tmp in str.split('^')]
+		return cmps
 
 	def doRun(self):
 		for tmp in self.filelist:
-			rows = makeRows('rsc/' + tmp)
-			jsonfile = re.sub(r'([a-z]+)\d{4}\d{2}\d{2}\.txt',r'\1.json',tmp)
+			filename = tmp + '20161202.txt'
+
+			rows = self.makeRows('rsc/' + filename)
+			#jsonfile = re.sub(r'([a-z]+)\d{4}\d{2}\d{2}\.txt',r'\1.json',tmp)
+			jsonfile = tmp + '.json'
 			print(jsonfile)
 			neolib.StrToFile(json.dumps(rows,ensure_ascii=False), 'json/'+jsonfile);
 
 
-def makeRows(rsrc):
-	str = neolib.StrFromFile(rsrc, enc='euc-kr')
-	cmps = [tmp.split('|') for tmp in str.split('^')]
-	return cmps
+class MakeColInfoFromJson(MakeJsonFormFromText):
+
+
+	def doRun(self):
+		self.mapCols = collections.OrderedDict()
+		for tmp in self.filelist:
+			jsonfile = 'json/'+tmp + '.json'
+			strcontnts = neolib.StrFromFile(jsonfile)
+			listlist = json.loads(strcontnts)
+
+			self.procListList(tmp,listlist)
+
+		self.procFinal()
+
+
+	def procListList(self,tablename,listlist):
+		print("{0}|{1}".format(tablename, ",".join(listlist[0])))
+		self.mapCols[tablename]=listlist[0]
+
+	def procFinal(self):
+
+		neolib.StrToFile(json.dumps(self.mapCols, ensure_ascii=False), 'json/colinfo.json' );
+		None
+
+class CheckMaxLen(MakeColInfoFromJson):
+	maxSize = 0
+
+	def procListList(self,tablename,listlist):
+
+		cols = listlist[0]
+		listlistcontents = listlist[1:len(listlist)]
+		print(tablename)
+		for row in listlistcontents:
+			#print(row)
+			for col in row:
+				#print(len(col))
+				self.maxSize = max(len(col),self.maxSize)
+
+
+
+	def procFinal(self):
+
+		print(self.maxSize)
+
+
+		None
+class InsertDBFromJson(MakeColInfoFromJson):
+	class MakeDB_BASE(neodb.BaseMySQLRunnable):
+		def InitRun(self):
+			self.dbaddress = self.mapArgs['dbaddress']
+			self.listmap = []
+
+			print("dst db:" + self.dbaddress)
+
+			self.dstdbHD = neodb.dbHandleing(host=self.dbaddress, port=3306, user='neo1seok', passwd='tofhdna1pi', db='kbus',
+									   charset='utf8')
+			None
+		def SetColine(self, colline):
+			self.colline = colline
+
+		def SetListList(self, listlistcontents):
+			self.listlistcontents = listlistcontents
+
+		def processInserValues(self):
+			idx = -1;
+			maxnum = len(self.listlistcontents)
+			for row in self.listlistcontents:
+				idx += 1
+				maprow = {self.cols[idx]: row[idx] for idx in range(0, len(self.cols))}
+				self.listmap.append(maprow)
+
+				if len(self.listmap) > 5000:
+					print('%s %d / %d'%(self.dsttable,idx,maxnum))
+					self.processInserToDB()
+					self.listmap.clear()
+
+
+		def doRun(self):
+			self.strlines = ""
+			if self.mapArgs['deleteTable']:
+				self.deleteTable()
+
+			self.cols = re.split(r',\s*', self.colline)
+			self.listmap = []
+
+			self.processInserValues()
+
+			for row in self.listmap:
+				print(row)
+			self.processInserToDB()
+
+			self.processAfterDB()
+
+			time.sleep(0.3)
+
+			None
+
+
+
+			None
+
+
+	class MakeDB_area(MakeDB_BASE):
+		dsttable = "area";
+		prefix = "ara"
+
+	class MakeDB_route(MakeDB_BASE):
+		dsttable = "route";
+		prefix = "rte"
+
+	class MakeDB_routeline(MakeDB_BASE):
+		dsttable = "routeline";
+		prefix = "rtl"
+
+	class MakeDB_routestation(MakeDB_BASE):
+		dsttable = "routestation";
+		prefix = "rts"
+
+	class MakeDB_station(MakeDB_BASE):
+		dsttable = "station";
+		prefix = "stn"
+
+	def InitRun(self):
+
+		self.mapfilelist =  {
+			'area':self.MakeDB_area,
+			'route':self.MakeDB_route,
+			'routeline':self.MakeDB_routeline,
+			'routestation':self.MakeDB_routestation,
+			'station':self.MakeDB_station
+		}
+
+		print(self.mapfilelist)
+
+
+	def procListList(self,tablename,listlist):
+		print(tablename)
+		dbRunhandler = self.mapfilelist[tablename](exit=False,dbaddress="neo1seok.iptime.org")
+		cols = listlist[0]
+		listlistcontents = listlist[1:len(listlist)]
+		dbRunhandler.SetColine(",".join(cols))
+		dbRunhandler.SetListList(listlistcontents)
+		dbRunhandler.Run()
+
+	def procFinal(self):
+
+		None
+
+
+
+class CreateTableAnd(neodb.MakeCreateTableFor):
+	xlsDbFile = "TABLE정보.xlsx"
+	def doRun(self):
+		ret = self.makeMapFromExcel(self.xlsDbFile)
+		self.strlines = self.makeSqlDropAndCreate(ret,self.createTableForm,self.fieldForm)
+		neolib.StrToFile(self.strlines, "table/TABLE.SQL")
+		self.strlines = self.makeSqlDropAndCreate(ret, self.dropTableForm, '')
+		neolib.StrToFile(self.strlines, "table/DROP.SQL")
+
+		None
 
 
 
 if __name__ != '__main__':
 	exit()
 
+InsertDBFromJson().Run()
+CreateTableAnd().Run()
+CheckMaxLen().Run()
+
+#CreateTableAnd().Run()
+
+MakeColInfoFromJson().Run()
 
 
+MakeJsonFormFromText().Run()
 
-
-
-
-#MakeJsonFormFromText().Run()
-
-TestOpeAPI().Run()
+#TestOpeAPI().Run()
 
 
 
--- test.py
+++ test.py
@@ -8,12 +8,12 @@ import re
 import shutil
 import socket
 import sys
+
 import win32api
-import win32gui
 import inspect
 import requests
 import  simplejson as json
-import win32con
+
 
 import neolib.neolib as neolib
 
@@ -45,8 +45,125 @@ class TestClass:
 		None
 
 import urllib.request
+from Crypto.Hash import SHA256
+
+#import winrandom
+
+str = neolib.StrFromFile('rsc/계좌.txt')
+lines = str.split('\r\n')
+listlist = [ [cols for cols in tmp.split('\t')  ] for tmp in str.split('\r\n') ]
+newlistlist = []
+for row  in listlist:
+	if len(row) <= 1:continue
+	if row[0] == '거래일자' or  row[4] == '0':continue
+
+	if int(row[4]) < 1000000 :continue
+	newlistlist.append(row)
+
+newstr = "\r\n".join([ "\t".join(row) for row  in newlistlist])
+print(newstr)
+neolib.StrToFile(newstr,'rsc/계좌new.txt')
+exit()
+import Crypto.Hash
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import ast
+import pyelliptic
+
+iv = pyelliptic.Cipher.gen_IV('aes-256-cfb')
+ctx = pyelliptic.Cipher("secretkey", iv, 1, ciphername='aes-256-cfb')
+
+ciphertext = ctx.update('test1')
+ciphertext += ctx.update('test2')
+ciphertext += ctx.final()
+print(ciphertext)
+ctx2 = pyelliptic.Cipher("secretkey", iv, 0, ciphername='aes-256-cfb')
+print(ctx2.ciphering(ciphertext))
 
 
+# Asymmetric encryption
+alice = pyelliptic.ECC() # default curve: sect283r1
+bob = pyelliptic.ECC(curve='sect571r1')
+
+ciphertext = alice.encrypt("Hello Bob", bob.get_pubkey(),
+                           ephemcurve='sect571r1')
+print(bob.decrypt(ciphertext))
+
+signature = bob.sign("Hello Alice")
+# alice's job :
+print(pyelliptic.ECC(pubkey=bob.get_pubkey(),
+                     curve='sect571r1').verify(signature, "Hello Alice"))
+
+
+exit()
+
+random_generator = Random.new().read
+key = RSA.generate(1024, random_generator)
+print(hex(key.key.n))
+print(type(key.key.e))
+print(key.key.d)
+
+
+publickey = key.publickey() # pub key export for exchange
+
+print(publickey)
+encrypted = publickey.encrypt('encrypt this message'.encode(), 32)
+print(encrypted)
+#message to encrypt is in the above line 'encrypt this message'
+
+print( 'encrypted message:', encrypted) #ciphertext
+
+f = open ('encryption.txt', 'w')
+f.write(str(encrypted)) #write ciphertext to file
+f.close()
+
+#decrypted code below
+
+f = open('encryption.txt', 'r')
+message = f.read()
+
+
+decrypted = key.decrypt(ast.literal_eval(str(encrypted)))
+
+print( 'decrypted', decrypted)
+
+f = open ('encryption.txt', 'w')
+f.write(str(message))
+f.write(str(decrypted))
+f.close()
+exit()
+hash = Crypto.Hash.SHA256.new()
+
+hash.update("Nobody inspects".encode())
+hash.update(" the spammish repetition".encode())
+
+print(neolib.ByteArray2HexString(hash.digest()))
+m = hashlib.sha256()
+m.update("Nobody inspects".encode())
+m.update(" the spammish repetition".encode())
+
+print(neolib.ByteArray2HexString(m.digest()))
+
+from Crypto.Cipher import AES
+
+obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+message = "The answer is no"
+
+ciphertext = obj.encrypt(message)
+
+print(neolib.ByteArray2HexString(ciphertext))
+
+
+obj2 = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+print(obj2.decrypt(ciphertext).decode())
+
+
+
+
+exit()
+import ctypes
+Win32DLL = ctypes.WinDLL("D:/PROJECT/ADVANCE/Win32DLL/x64/Debug/Win32DLL.dll")
+Win32DLL.fnWin32DLL()
 
 exit()
 print(re.match(r'asdsa',"adsaf"))
